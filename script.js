477249300c38ffe453d542d69ad1b7749e10c55e
Elimarcos De Souza Pereira <110783938+ElimarcosDev@users.noreply.github.com>
2022-11-12 14:54:41 -0300

    finalizando o projeto

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
 
-function tema(){
-    var dark = document.getElementById('dark')
-    var white = document.getElementById('white')
+function white(){
     
-    if (dark = 'checked'){
-        document.querySelector("link[rel='shortcut icon']").href = "https://emojis.wiki/emoji-pics/microsoft/new-moon-microsoft.png";
-        
-        
-        
-    }
-    else if (white = 'checked'){
-        document.body.style.backgroundColor = 'white';
-        document.querySelector("link[rel='shortcut icon']").href = "https://static.vecteezy.com/ti/vetor-gratis/p1/627460-sol-simbolo-sinal-simbolo-gr%C3%A1tis-vetor.jpg";
-    }
+    var white = document.getElementsByName('white')
+    document.querySelector("link[rel='shortcut icon']").href = "https://static.vecteezy.com/ti/vetor-gratis/p1/627460-sol-simbolo-sinal-simbolo-gr%C3%A1tis-vetor.jpg";
+    document.body.style.backgroundColor = 'white'
+    document.querySelector("title").innerHTML = "White";
 
-    
+}
+
+function dark(){
+    var dark = document.getElementsByName('dark')
+    document.querySelector("link[rel='shortcut icon']").href = "https://emojis.wiki/emoji-pics/microsoft/new-moon-microsoft.png";
+    document.body.style.backgroundColor = '#363636'
+    document.querySelector("title").innerHTML = "Black";
 }
 
