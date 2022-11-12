2f55c25665ade29322f633c3681982df7f7b67a9
Elimarcos De Souza Pereira <110783938+ElimarcosDev@users.noreply.github.com>
2022-11-12 15:08:37 -0300

    inicio da criação do site de exemplo

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ function white(){
     
     var white = document.getElementsByName('white')
     document.querySelector("link[rel='shortcut icon']").href = "https://static.vecteezy.com/ti/vetor-gratis/p1/627460-sol-simbolo-sinal-simbolo-gr%C3%A1tis-vetor.jpg";
-    document.body.style.backgroundColor = 'white'
+    document.body.style.backgroundColor = 'aliceblue'
     document.querySelector("title").innerHTML = "White";
-
+    
 }
 
 function dark(){
