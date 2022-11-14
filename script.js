b4c4eefe943c73edb0bf4b74a04dbc351a1c01a9
Elimarcos De Souza Pereira <110783938+ElimarcosDev@users.noreply.github.com>
2022-11-14 10:37:09 -0300

    finalizei o meu teste

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 
 function white(){
-    
     var white = document.getElementsByName('white')
-    document.querySelector("link[rel='shortcut icon']").href = "https://static.vecteezy.com/ti/vetor-gratis/p1/627460-sol-simbolo-sinal-simbolo-gr%C3%A1tis-vetor.jpg";
+    var menu = document.getElementById('tema')
+    var titulo = document.getElementById('principal')
+    var paragrafo = document.getElementById('txt')
+    document.querySelector("link[rel='shortcut icon']").href = "https://i.pinimg.com/originals/02/ed/f4/02edf4e428d6fedc372e8d4fc7b25d6e.png";
     document.body.style.backgroundColor = 'aliceblue'
     document.querySelector("title").innerHTML = "White";
-    
+    titulo.style.color = 'black'
+    paragrafo.style.color= 'black'
+    menu.style.display = 'none'
 }
 
 function dark(){
     var dark = document.getElementsByName('dark')
+    var menu = document.getElementById('tema')
+    var titulo = document.getElementById('principal')
+    var paragrafo = document.getElementById('txt')
     document.querySelector("link[rel='shortcut icon']").href = "https://emojis.wiki/emoji-pics/microsoft/new-moon-microsoft.png";
     document.body.style.backgroundColor = '#363636'
     document.querySelector("title").innerHTML = "Black";
+    titulo.style.color = 'white'
+    paragrafo.style.color= 'white'
+    menu.style.display = 'none'
 }
 function menu(){
     var menu = document.getElementById('tema')
@@ -24,6 +34,11 @@ function menu(){
     else{
         menu.style.display = 'inline-block'
     }
+    
+}
+function main(){
+    var menu = document.getElementById('tema')
+    menu.style.display = 'none'
     
 }
 
